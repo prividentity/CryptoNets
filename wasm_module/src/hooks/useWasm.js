2843a9c7b29b4bb3d0487a4c42b03f03d07bf6e8
Clark Evangelista <cjamesevangelista@gmail.com>
2022-12-21 21:27:40 +0800

    barcode fixes to main

--- wasm_module/src/hooks/useWasm.js
+++ wasm_module/src/hooks/useWasm.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { loadPrivIdModule } from '@privateid/cryptonets-web-sdk-alpha';
+import { loadPrivIdModule } from '@privateid/cryptonets-web-sdk';
 
 const useWasm = () => {
   // Initialize the state
