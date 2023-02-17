65270ce6f0cf70a0644ebcf573dcbe3eb129dc6c
Clark Evangelista <cjamesevangelista@gmail.com>
2023-02-17 23:23:46 +0800

    setting npm package to alpha

--- wasm_module/src/hooks/usePredictOneFa.js
+++ wasm_module/src/hooks/usePredictOneFa.js
@@ -16,7 +16,7 @@ const usePredictOneFa = (element = "userVideo", onSuccess, retryTimes = 4, devic
       callback,
       {
         input_image_format: "rgba",
-        send_original_images: false,
+        send_original_images: true,
       },
     );
   };
@@ -34,12 +34,6 @@ const usePredictOneFa = (element = "userVideo", onSuccess, retryTimes = 4, devic
           setShowSuccess(true);
         }
         if (result.returnValue?.status !== 0) {
-          if (tries === retryTimes) {
-            // onFailure();
-          } else {
-            tries += 1;
-            // enrollUserOneFa();
-          }
           const {validation_status, message}  = result.returnValue;
           setPredictMessage(message);
           let hasValidFace =false;
@@ -51,6 +45,7 @@ const usePredictOneFa = (element = "userVideo", onSuccess, retryTimes = 4, devic
           }
           setFaceDetected(hasValidFace);
           setPredictStatus(null);
+          predictUserOneFa();
         }
         break;
       default:
