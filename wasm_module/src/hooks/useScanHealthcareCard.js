72c2b4ac0ababab2d4c67181b23186701d3892ad
Moez Bouselmi <Moez.Bouselmi@gmail.com>
2023-05-23 23:07:52 +0200

    wip

--- wasm_module/src/hooks/useScanHealthcareCard.js
+++ wasm_module/src/hooks/useScanHealthcareCard.js
@@ -29,9 +29,21 @@ const useScanHealthcareCard = (setShowSuccess = () => {}) => {
   const callback = (result) => {
     console.log("Healthcare Card Scan Result:", result);
 
+    console.log('======= Healthcare Card Scan Result: ==================');
+    console.log('result.returnValue.conf_level : ' + result.returnValue.conf_level);
+    console.log('result.op_status: ' + result.returnValue.op_status);
+    console.log('======================================================');
+
+    if (result.returnValue.conf_level < 0.8 && result.op_status===0){
+      console.log('=======PROBLEMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM==================\n');
+      console.log(result);
+      console.log('======================================================\n');
+    }
+
     if (
       result.returnValue.op_status === 0 &&
       //result.returnValue.conf_level > 0.95 &&
+      
       result.returnValue.cropped_doc_width
     ) {
       setShowSuccess(true);
