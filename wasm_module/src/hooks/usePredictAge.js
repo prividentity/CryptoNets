574f9ee131a285efbb0d40becefae2a7fef07624
Clark Evangelista <cjamesevangelista@gmail.com>
2023-09-11 20:41:00 +0800

    api v2 to main

--- wasm_module/src/hooks/usePredictAge.js
+++ wasm_module/src/hooks/usePredictAge.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { predictAge } from "@privateid/cryptonets-web-sdk-alpha";
+import { predictAge } from "@privateid/cryptonets-web-sdk";
 
 const usePredictAge = () => {
   const [age, setAge] = useState(null);
