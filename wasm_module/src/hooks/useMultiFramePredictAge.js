28756df0487a1e1160dea926d1ab5b9f9dd8db6b
Clark Evangelista <cjamesevangelista@gmail.com>
2023-11-15 21:56:08 +0800

    reduced greyscale threshold

--- wasm_module/src/hooks/useMultiFramePredictAge.js
+++ wasm_module/src/hooks/useMultiFramePredictAge.js
@@ -46,6 +46,8 @@ const useMultiFramePredictAge = () => {
     await predictAge(mfCallback, {
       skip_antispoof: skipAntispoofGlobal,
       mf_token: mfToken,
+      gray_scale_variance_threshold: 100.0,
+      gray_scale_threshold: 14,
     });
   };
 
