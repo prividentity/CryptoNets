6de4ec38d1f9847317f803520f37462c5a416e70
Clark Evangelista <cjamesevangelista@gmail.com>
2023-12-15 03:44:32 +0800

    age update

--- wasm_module/src/hooks/useDelete.js
+++ wasm_module/src/hooks/useDelete.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { deleteUser } from "@privateid/cryptonets-web-sdk-alpha";
+import { deleteUser } from "@privateid/cryptonets-web-sdk";
 
 // useDelete
 const useDelete = (onDeleteEnd, ready) => {
