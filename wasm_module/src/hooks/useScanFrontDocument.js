d5bbf02631d34a20b4efc0f9a191e2282add1382
Clark Evangelista <cjamesevangelista@gmail.com>
2024-12-04 21:26:08 +0800

    new threshold

--- wasm_module/src/hooks/useScanFrontDocument.js
+++ wasm_module/src/hooks/useScanFrontDocument.js
@@ -104,7 +104,7 @@ const useScanFrontDocumentWithoutPredict = (setShowSuccess) => {
       callback: documentCallback,
       image: uploadData,
       config: {
-        blur_threshold_doc: 1200
+        blur_threshold_doc: 1500
       }
     });
 
